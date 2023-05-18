527e3167e46de5c78a69bc2535ab829be257f465
DDR13GIT <ddroy13@gmail.com>
2023-05-18 22:42:43 +0600

    Feat: settings_activity.xml design change to large topappbar used sharedPref to save data of count

--- app/src/main/java/com/ddroy/tapcounter/MainActivity.kt
+++ app/src/main/java/com/ddroy/tapcounter/MainActivity.kt
@@ -1,67 +1,70 @@
+@file:Suppress("DEPRECATION")
+
 package com.ddroy.tapcounter
 
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
-import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.VibrationEffect
 import android.os.Vibrator
-import android.widget.Button
-import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
+import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceManager
 import com.ddroy.tapcounter.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var sharedPreferences: SharedPreferences
     private var count = 0
     private var flag = 0
     private lateinit var txt: TextView
-    private lateinit var img: ImageView
-
-
     private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        userSettings()
-
-
-        val decreaseButton = findViewById<Button>(R.id.decreaseCountBtn)
-        val resetButton = findViewById<Button>(R.id.resetBtn)
-        val lockButton = findViewById<Button>(R.id.lockBtn)
         txt = findViewById(R.id.countTxt)
-        img = findViewById(R.id.imageView3)
 
-        img.setOnClickListener {
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        count = sharedPreferences.getInt("count", 0)
+        txt.text = count.toString()
+
+        binding.imageView3.setOnClickListener {
             if (flag == 0) {
                 count++
                 txt.text = count.toString()
                 textAnimation()
                 checkAndPlayVibration()
+
+
             }
         }
 
-        decreaseButton.setOnClickListener {
+        binding.decreaseCountBtn.setOnClickListener {
             if (flag == 0) {
                 count--
+
+
                 updateCountText()
             }
         }
 
-        resetButton.setOnClickListener {
+        binding.resetBtn.setOnClickListener {
             count = 0
+
+
             updateCountText()
         }
 
-        lockButton.setOnClickListener {
+            binding.lockBtn.setOnClickListener {
             when (flag) {
                 0 -> {
                     flag = 1
@@ -75,11 +78,10 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        val topAppBar =
-            findViewById<com.google.android.material.appbar.MaterialToolbar>(R.id.topAppBar)
 
 
-        topAppBar.setOnMenuItemClickListener { menuItem ->
+
+        binding.topAppBar.setOnMenuItemClickListener { menuItem ->
             when (menuItem.itemId) {
 
                 R.id.settings -> {
@@ -94,23 +96,11 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-//    private fun userSettings(){
-//    val prefs = PreferenceManager.getDefaultSharedPreferences(this)
-//        val sound_switch_key = prefs.getBoolean("sound_switch_key", false)
-//        val vibration_switch_key = prefs.getBoolean("vibration_switch_key", false)
-//        val volume_switch_key = prefs.getBoolean("volume_switch_key", false)
-//
-//        binding.apply {
-//            if(sound_switch_key){
-//
-//            }
-//        }
-//    }
-
     private fun updateCountText() {
         val builder = StringBuilder()
         builder.append(count)
         txt.text = builder.toString()
+        sharedPreferences.edit().putInt("count", count).apply()
         textAnimation()
     }
 
@@ -127,32 +117,42 @@ class MainActivity : AppCompatActivity() {
 
     private fun checkAndPlayVibration() {
         val prefs = PreferenceManager.getDefaultSharedPreferences(this)
-        val vibration_switch_key = prefs.getBoolean("vibration_switch_key", false)
+        val vibrationEnabled  = prefs.getBoolean("vibration_switch_key", false)
 
         binding.apply {
-            if (vibration_switch_key) {
-                val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+            if (vibrationEnabled) {
+                val vibrator = getSystemService(VIBRATOR_SERVICE) as Vibrator
 
                 // Check if the device supports vibration
                 if (vibrator.hasVibrator()) {
-                    // Vibrate for 100 milliseconds
-                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                        vibrator.vibrate(
-                            VibrationEffect.createOneShot(100, VibrationEffect.DEFAULT_AMPLITUDE)
-                        )
-                    } else {
-                        val vibrationEffect = VibrationEffect.createOneShot(100, 50)
-                        vibrator.vibrate(vibrationEffect)
-                    }
+                    val milliseconds = 100L
+                    val amplitude = VibrationEffect.DEFAULT_AMPLITUDE
+                    val effect = VibrationEffect.createOneShot(milliseconds, amplitude)
+
+                    vibrator.vibrate(effect)
                 }
             }
         }
 
     }
 
-}
+    override fun onStop() {
+        super.onStop()
+        // Save the count value when the app is closed
+        sharedPreferences.edit().putInt("count", count).apply()
+    }
 
-// trying vibration on button click
-// need to store the count value using shared preferences
+//    fun playTapSound() {
+//        // Get the sound resource
+//        val soundResId = R.raw.tap_sound
+//
+//        // Create a MediaPlayer object
+//        val mediaPlayer = MediaPlayer.create(this, soundResId)
+//
+//        // Play the sound
+//        mediaPlayer.start()
+//    }
+
+}
 
 
