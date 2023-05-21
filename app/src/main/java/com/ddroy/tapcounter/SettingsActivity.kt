97fe228d5acb6760794a8373d466bcbf551f537c
DDR13GIT <ddroy13@gmail.com>
2023-05-21 23:45:32 +0600

    Feat: function to rate the app added

--- app/src/main/java/com/ddroy/tapcounter/SettingsActivity.kt
+++ app/src/main/java/com/ddroy/tapcounter/SettingsActivity.kt
@@ -1,13 +1,17 @@
 package com.ddroy.tapcounter
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.preference.PreferenceFragmentCompat
 import com.google.android.material.appbar.MaterialToolbar
 
 class SettingsActivity : AppCompatActivity() {
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
@@ -23,8 +27,23 @@ class SettingsActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        val button = findViewById<Button>(R.id.rateAppBtn)
+        button.setOnClickListener {
+            // Call the function to rate the app.
+            rateApp()
+        }
     }
 
+    private fun rateApp() {
+        // Get the package name of the running app.
+        val packageName = getPackageName()
+
+        // Create an intent to open the app rating page in the Google Play Store.
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$packageName"))
+
+        // Start the intent.
+        startActivity(intent)
+    }
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
